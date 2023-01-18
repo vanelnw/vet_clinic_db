af38cff401212e74f886b718e4599e8ba05dbd2d
Vanel Nwaba <vanelnwaba@gmail.com>
2023-01-18 11:46:39 +0100

    alter awners and species tables

--- schema.sql
+++ schema.sql
@@ -23,3 +23,9 @@ CREATE TABLE species (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
+
+/* alter tables*/
+ALTER TABLE animals ADD PRIMARY KEY (id);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INTEGER REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INTEGER REFERENCES owners(id);
