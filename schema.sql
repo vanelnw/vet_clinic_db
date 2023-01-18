f692fc0d7a83b2dec16599008edeb4e54b4266e9
Vanel Nwaba <vanelnwaba@gmail.com>
2023-01-18 18:24:01 +0100

    create tables vets, specializations and visits

--- schema.sql
+++ schema.sql
@@ -29,3 +29,24 @@ ALTER TABLE animals ADD PRIMARY KEY (id);
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD species_id INTEGER REFERENCES species(id);
 ALTER TABLE animals ADD owner_id INTEGER REFERENCES owners(id);
+
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(150) NOT NULL,
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    species_id INTEGER REFERENCES species (id),
+    vet_id INTEGER REFERENCES vets (id),
+    PRIMARY KEY (species_id, vet_id)
+);
+
+CREATE TABLE visits (
+    animal_id INTEGER REFERENCES animals (id),
+    vet_id INTEGER REFERENCES vets (id),
+    visit_date TIMESTAMP NOT NULL,
+    PRIMARY KEY (animal_id, vet_id, visit_date)
+);
